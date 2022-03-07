ce4b455ebd7d95f13a6ef8968d4fb80ced9e0aa8
JazminLoM <94807623+JazminLoM@users.noreply.github.com>
2022-03-07 00:00:44 -0600

    Adaptador Personalizado

---
+++ app/src/main/java/com/example/ejemplolistview/Fruta.kt
@@ -0,0 +1,13 @@
+package com.example.ejemplolistview
+
+import android.media.Image
+
+class fruta(nombre:String, imagen: Int) {
+    var nombre:String = ""
+    var imagen:Int = 0
+
+    init {
+        this.nombre = nombre
+        this.imagen = imagen
+    }
+}
---
+++ app/src/main/java/com/example/ejemplolistview/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.ejemplolistview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var frutas:ArrayList<fruta> = ArrayList()
+        frutas.add(fruta("Manzana", R.drawable.manzana ) )
+        frutas.add(fruta("Durazno", R.drawable.durazno))
+        frutas.add(fruta("Platano", R.drawable.platano))
+        frutas.add(fruta("Sandia", R.drawable.sandia))
+
+        val lista = findViewById<ListView>(R.id.lista)
+
+        val adapatador = AdaptadorCustom(this, frutas)
+        lista.adapter  =adapatador
+
+        lista.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, i, l ->
+            Toast.makeText(this, frutas.get(i).nombre, Toast.LENGTH_LONG).show()
+        }
+
+
+
+
+    }
+}
